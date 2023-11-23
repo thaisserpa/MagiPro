b9658fa527bd8febf7213b6203e38cbec79e3f3f
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-11-23 08:49:30 -0300

    Branch frontend created and its first commit

---
+++ candidatura/index.js
@@ -0,0 +1,8 @@
+if(formulario !== null && formulario !== ''){
+    document.getElementsById("submit").desabled=false;
+  
+  } else{
+    document.getElementById("reset").disabled=true;
+  }
+  
+  
