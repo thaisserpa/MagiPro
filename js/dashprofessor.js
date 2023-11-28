59621264c99ef04db68ed25a8f9b575d015c1686
RodrigoMSCruz <rodrigomoraessantacruz@yahoo.com.br>
2023-11-27 22:30:55 -0300

    Commit final de 2023-11-27 - Oficial da apresentacao

--- js/dashprofessor.js
+++ js/dashprofessor.js
@@ -17,10 +17,9 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(projects => {
         console.log('Projetos recebidos da API:', projects);
 
-        // Limpar o conteúdo atual do contêiner de projetos
+      
         projectsContainer.innerHTML = '';
 
-        // Verificar se há projetos
         if (projects.projects && projects.projects.length > 0) {
             // Iterar sobre os projetos e criar elementos HTML para cada um
             projects.projects.forEach(project => {
@@ -58,8 +57,6 @@ function createProjectElement(projectName) {
     const buttonElement = document.createElement('button');
     buttonElement.textContent = 'Acessar';
 
-    // Adicione outros elementos conforme necessário...
-
     detailsElement.appendChild(titleElement);
     detailsElement.appendChild(buttonElement);
 
@@ -68,3 +65,72 @@ function createProjectElement(projectName) {
 
     return projectElement;
 }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const apiUrl = 'http://89.117.75.191:8070/applications';
+
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+        console.error('Token de autenticação não encontrado.');
+        return;
+    }
+
+    fetch(apiUrl, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const tabela = document.getElementById('sua-tabela');
+
+            // Verifica se a propriedade 'applications' existe e é uma matriz
+            if (Array.isArray(data.applications)) {
+                data.applications.forEach(item => {
+                    const tr = document.createElement('tr');
+
+                    const td1 = document.createElement('td');
+                    td1.textContent = item.summary;  
+                    tr.appendChild(td1);
+
+                    const td2 = document.createElement('td');
+                    td2.textContent = item.resumePath;  
+                    tr.appendChild(td2);
+
+                    const td4 = document.createElement('td');
+                    const aceitarLink = document.createElement('a');
+                    aceitarLink.id = 'aceitar';
+                    aceitarLink.href = '#';
+                    aceitarLink.className = 'btn';
+                    aceitarLink.textContent = 'Aceitar';
+                    td4.appendChild(aceitarLink);
+                    tr.appendChild(td4);
+
+                    const td5 = document.createElement('td');
+                    const recusarLink = document.createElement('a');
+                    recusarLink.id = 'recusar';
+                    recusarLink.href = '#';
+                    recusarLink.className = 'btn';
+                    recusarLink.textContent = 'Recusar';
+                    td5.appendChild(recusarLink);
+                    tr.appendChild(td5);
+
+                    tabela.appendChild(tr);
+                });
+            } else {
+                console.error('A propriedade "applications" não é uma matriz ou não existe nos dados recebidos.');
+            }
+        })
+        .catch(error => {
+            console.error('Erro na requisição:', error.message);
+        });
+});
